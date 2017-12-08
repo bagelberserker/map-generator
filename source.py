669e165388eae7af2eff20eaa8954978c2acd3dc
bagelberserker <34356913+bagelberserker@users.noreply.github.com>
2017-12-07 21:12:18 -0600

    Update source.py
    Cleaned up some typos and formatting. Modified the non-island walk algorithm to (hopefully) be more realistic.

--- source.py
+++ source.py
@@ -6,7 +6,7 @@ output = []
 
 
 def build():
-  """Creates the map using the declared height and width."""
+	"""Creates the map using the declared height and width."""
 	for n in range(MAPWIDTH):
 		output.append([])
 	for x in output:
@@ -15,8 +15,8 @@ def build():
 
 
 def walk(island=True):
-  """Puts a 'walker' at the center of the map, then moves the walker 1 step in a random cardinal direction.
-     After each step, the walker increments the value of the space it is on by 1."""
+	"""Puts a 'walker' at the center of the map, then moves the walker 1 step in a random cardinal direction.
+		After each step, the walker increments the value of the space it is on by 1."""
 	xcoordinate = int(MAPWIDTH/2)
 	ycoordinate = int(MAPHEIGHT/2)
 	output[xcoordinate][ycoordinate] += 1
@@ -30,14 +30,22 @@ def walk(island=True):
 			ycoordinate -= 1
 		else:
 			xcoordinate -= 1
-		if island:  # Simulates a volcanic island by reseting thhe walker to the starting point whenever it would reach the edge.
-      if xcoordinate < 1 or xcoordinate >= (MAPWIDTH-1) or ycoordinate < 1 or ycoordinate >= (MAPHEIGHT-1):  # Looks clunky. Surely there's a shortcut/cleaner way?
-	  		xcoordinate, ycoordinate = int(MAPWIDTH/2), int(MAPHEIGHT/2)
-    else:  # Simulates landmasses of a larger scale, such as continents.
-      if xcoordinate < 1 or xcoordinate >= (MAPWIDTH-1):
-        xcoordinate = randint(int(MAPWIDTH/4), int(3*MAPWIDTH/4))
-      elif ycoordinate < 1 or ycoordinate >= (MAPHEIGHT-1):
-        ycoordinate = randint(int(MAPHEIGHT/4), int(3*MAPHEIGHT/4))
+		if island:  # Simulates a volcanic island. When the walker would reach the edge, it's location is reset to the starting point.
+  			if xcoordinate < 1 or xcoordinate >= (MAPWIDTH-1) or ycoordinate < 1 or ycoordinate >= (MAPHEIGHT-1):  # Looks clunky. Surely there's a shortcut/cleaner way?
+				xcoordinate, ycoordinate = int(MAPWIDTH/2), int(MAPHEIGHT/2)
+			else:
+				pass
+		else:  # Attempts to simulate landmasses of a large scale, such as continents. When the walker would reach the edge, it's location along that axis is semi-randomized.
+			if xcoordinate < 1:
+				xcoordinate = randint(int(MAPWIDTH/4), int(MAPWIDTH/2))
+			elif xcoordinate >= (MAPWIDTH-1):
+				xcoordinate = randint(int(MAPWIDTH/2), int(3*MAPWIDTH/4))
+			elif ycoordinate < 1:
+				ycoordinate = randint(int(MAPHEIGHT/4), int(MAPHEIGHT/2))
+			elif ycoordinate >= (MAPHEIGHT-1):
+				ycoordinate = randint(int(MAPHEIGHT/2), int(3*MAPHEIGHT/4))
+			else:
+				pass
 		output[xcoordinate][ycoordinate] += 1
 
 
