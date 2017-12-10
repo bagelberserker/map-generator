ad62a48101d521d99b0177fb6bb773e97fe5eae9
chasebaker288 <chasebaker288@gmail.com>
2017-12-10 14:50:32 -0600

    Update source.py
    Removed continent feature

--- source.py
+++ source.py
@@ -20,7 +20,6 @@ def walk():
 		After each step, the walker increments the value of the space it is on by 1."""
 	xcoordinate = int(MAPWIDTH/2)
 	ycoordinate = int(MAPHEIGHT/2)
-	output[ycoordinate][xcoordinate] += 1
 	for n in range(MAPWIDTH*MAPHEIGHT*2):  # Total number of steps is equal to area*2, so the final map has an average of 2 steps per 'tile'.
 		direction = randint(1, 4)
 		if direction == 1:
@@ -31,22 +30,10 @@ def walk():
 			ycoordinate -= 1
 		else:
 			xcoordinate -= 1
-		if ISISLAND:  # Simulates a volcanic island. When the walker would reach the edge, it's location is reset to the starting point.
-  			if xcoordinate < 1 or xcoordinate >= (MAPWIDTH-1) or ycoordinate < 1 or ycoordinate >= (MAPHEIGHT-1):  # Looks clunky. Surely there's a shortcut/cleaner way?
-				xcoordinate, ycoordinate = int(MAPWIDTH/2), int(MAPHEIGHT/2)
-			else:
-				pass
-		else:  # Attempts to simulate landmasses of a large scale, such as continents. When the walker would reach the edge, it's location along that axis is semi-randomized.
-			if xcoordinate < 1:
-				xcoordinate = randint(int(MAPWIDTH/4), int(MAPWIDTH/2))
-			elif xcoordinate >= (MAPWIDTH-1):
-				xcoordinate = randint(int(MAPWIDTH/2), int(3*MAPWIDTH/4))
-			elif ycoordinate < 1:
-				ycoordinate = randint(int(MAPHEIGHT/4), int(MAPHEIGHT/2))
-			elif ycoordinate >= (MAPHEIGHT-1):
-				ycoordinate = randint(int(MAPHEIGHT/2), int(3*MAPHEIGHT/4))
-			else:
-				pass
+		if xcoordinate < 1 or xcoordinate >= (MAPWIDTH-1) or ycoordinate < 1 or ycoordinate >= (MAPHEIGHT-1):  # Looks clunky. Surely there's a shortcut/cleaner way?
+			xcoordinate, ycoordinate = int(MAPWIDTH/2), int(MAPHEIGHT/2)
+		else:
+			pass
 		output[ycoordinate][xcoordinate] += 1
 
 
