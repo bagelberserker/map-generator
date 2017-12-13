e8d30ba39cc14b226cce77f22cfddbb028ea186e
chasebaker288 <chasebaker288@gmail.com>
2017-12-13 15:07:46 -0600

    Update source.py

--- source.py
+++ source.py
@@ -2,25 +2,27 @@ from random import randint
 
 MAPWIDTH = 25
 MAPHEIGHT = 25
-ISISLAND = True
-output = []
 
 
-def build():
+def build(width, height):
 	"""Creates the map using the declared height and width."""
-	for n in range(MAPHEIGHT):
+	output = []
+	for n in range(height):
 		output.append([])
 	for x in output:
-		for y in range(MAPWIDTH):
+		for y in range(width):
 			x.append(0)
+	return output
 
 
-def walk():
+def walk(empty_map):
 	"""Puts a 'walker' at the center of the map, then moves the walker 1 step in a random cardinal direction.
 		After each step, the walker increments the value of the space it is on by 1."""
-	xcoordinate = int(MAPWIDTH/2)
-	ycoordinate = int(MAPHEIGHT/2)
-	for n in range(MAPWIDTH*MAPHEIGHT*2):  # Total number of steps is equal to area*2, so the final map has an average of 2 steps per 'tile'.
+	HEIGHT = len(empty_map)
+	WIDTH = len(empty_map[0])
+	xcoordinate = int(WIDTH/2)
+	ycoordinate = int(HEIGHT/2)
+	for n in range(WIDTH*HEIGHT*2):  # Total number of steps is equal to area*2, so the final map has an average of 2 steps per 'tile'.
 		direction = randint(1, 4)
 		if direction == 1:
 			ycoordinate += 1
@@ -30,20 +32,22 @@ def walk():
 			ycoordinate -= 1
 		else:
 			xcoordinate -= 1
-		if xcoordinate < 1 or xcoordinate >= (MAPWIDTH-1) or ycoordinate < 1 or ycoordinate >= (MAPHEIGHT-1):  # Looks clunky. Surely there's a shortcut/cleaner way?
-			xcoordinate, ycoordinate = int(MAPWIDTH/2), int(MAPHEIGHT/2)
+		if xcoordinate < 1 or xcoordinate >= (WIDTH-1) or ycoordinate < 1 or ycoordinate >= (HEIGHT-1):  # Looks clunky. Surely there's a shortcut/cleaner way?
+			xcoordinate, ycoordinate = int(WIDTH/2), int(HEIGHT/2)
 		else:
 			pass
-		output[ycoordinate][xcoordinate] += 1
+		empty_map[ycoordinate][xcoordinate] += 1
+	return empty_map
 
 
-build()
-walk()
-for i in range(MAPHEIGHT):  # Printed this way for ease of data usage during testing.
+ISLAND = build(MAPWIDTH, MAPHEIGHT)
+FINAL_RESULT = walk(ISLAND)
+
+for i in range(len(FINAL_RESULT)):  # Printed this way for ease of data usage during testing.
 	row = ""
-	for j in range(MAPWIDTH):
-		row += str(output[i][j])
-		if j < MAPWIDTH - 1:
+	for j in range(len(FINAL_RESULT[0])):
+		row += str(FINAL_RESULT[i][j])
+		if j < len(FINAL_RESULT[0]) - 1:
 			row += "	"
 		else:
 			pass
